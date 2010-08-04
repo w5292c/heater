83ea67387080057afa67de9515c4c23670eaba68
Alexander Chumakov <w5292c@gmail.com>
2010-08-04 19:00:35 +0400

    Implemented displaying a character in the test application

--- src/main.c
+++ src/main.c
@@ -3,10 +3,19 @@
 #include <stdio.h>
 
 int main (char **argv, int argn) {
-    muword i;
+    muword i, j;
     for (i = 0; i < 14; i++) {
-        mubyte bt = get_font14_byte (100, i);
-        printf ("Value: %d\n", bt);
+        mubyte bt = get_font14_byte (57, i);
+        printf ("[");
+        for (j = 0x80U; j != 0; j = j >>1) {
+            if (bt & j) {
+                printf ("XX");
+            }
+            else {
+                printf ("  ");
+            }
+        }
+        printf ("]\n");
     }
 
     return 0;
