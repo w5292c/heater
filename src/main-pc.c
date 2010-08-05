32c80fc5dc6d1f0dfedddf3d867bd72ae6913e69
Alexander Chumakov <w5292c@gmail.com>
2010-08-05 11:21:45 +0400

    Implemented displaying characters entered by the user

--- src/main-pc.c
+++ src/main-pc.c
@@ -3,20 +3,31 @@
 #include <stdio.h>
 
 int main (char **argv, int argn) {
-    muword i, j;
-    for (i = 0; i < 14; i++) {
-        mubyte bt = get_font14_byte (126, i);
-        printf ("[");
-        for (j = 0x80U; j != 0; j = j >>1) {
-            if (bt & j) {
-                printf ("XX");
+
+    do {
+        muword i, j;
+        int32_t enter;
+
+        printf ("Enter the code to be displayed: ");
+        scanf ("%d", &enter);
+        printf ("\nThe char: [%d]\n", enter);
+
+        for (i = 0; i < 14; i++) {
+            mubyte bt = get_font14_byte (enter, i);
+            printf ("[%2.2x] - ", bt);
+            printf ("[");
+            for (j = 0x80U; j != 0; j = j >>1) {
+                if (bt & j) {
+                    printf ("XX");
+                }
+                else {
+                    printf ("  ");
+                }
             }
-            else {
-                printf ("  ");
-            }
+            printf ("]\n");
         }
-        printf ("]\n");
     }
+    while (1);
 
     return 0;
 }
