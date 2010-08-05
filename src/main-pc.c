44ef7869efc778bf6aa0c4a7c9046dfebb49085f
Alexander Chumakov <w5292c@gmail.com>
2010-08-05 11:03:12 +0400

    Added 2 main functions for AVR and PC targets

---
+++ src/main-pc.c
@@ -0,0 +1,22 @@
+#include "fonts.h"
+
+#include <stdio.h>
+
+int main (char **argv, int argn) {
+    muword i, j;
+    for (i = 0; i < 14; i++) {
+        mubyte bt = get_font14_byte (126, i);
+        printf ("[");
+        for (j = 0x80U; j != 0; j = j >>1) {
+            if (bt & j) {
+                printf ("XX");
+            }
+            else {
+                printf ("  ");
+            }
+        }
+        printf ("]\n");
+    }
+
+    return 0;
+}
