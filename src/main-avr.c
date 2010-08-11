3c305031e3899567cac8140fbf5f0660e265e467
Alexander Chumakov <w5292c@gmail.com>
2010-08-11 08:29:43 +0400

    Fixed compilation error

--- src/main-avr.c
+++ src/main-avr.c
@@ -1,9 +1,13 @@
 #include "engine.h"
+#include "macros.h"
 #include "scheduler.h"
 
 #include <avr/interrupt.h>
 
 int main (int argc, char **argv) {
+    M_UNUSED_PARAM (argc);
+    M_UNUSED_PARAM (argv);
+
     /* initialize the engine */
     engine_init ();
 
